8ecace7309a40bc13a2b03a9529866749ff21284
theGoodBuyer <jamesonlee125@gmail.com>
2014-12-26 15:36:22 -0800

    adding files and consolidating the benchmark function

---
+++ jameson_test_main.py
@@ -0,0 +1,15 @@
+import collections
+import csv
+from datetime import datetime
+from BloombergClass import Bloomberg
+from ticker import Ticker
+import operator
+
+def main():
+	bloomberg = Bloomberg()
+	bloomberg.initializeData("exampleData.csv")
+
+	print sorted(bloomberg.tickerObjects["SPX Index"].date_to_performance.items(), key=operator.itemgetter(0))
+	# sorted(self.tickerObjects["SPX Index"].date_to_performance.items(), key=operator.itemgetter(0))
+
+main()
